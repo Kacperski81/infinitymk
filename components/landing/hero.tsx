5a703b54dd3e3d4da2b5430384899b358409b95d
Kacperski81 <kacperski81@gmail.com>
2026-01-21 12:39:59 +0000

    hero image overlay

--- components/landing/hero.tsx
+++ components/landing/hero.tsx
@@ -11,9 +11,11 @@ export default function Hero() {
                     priority
                     fill
                     sizes="100vw"
-                    className="h-full w-full object-cover object-center lg:p-2"
+                    className="h-full w-full object-cover object-center lg:p-2 saturate-[1.2] contrast-[1.05]"
                 />
-                {/* <div className="absolute inset-0 bg-gradient-to-r from-(--main-450)/60 via-(--main-450)/50 to-(--main-450)/70 lg:m-2"></div> */}
+                {/* Overlay */}
+                <div className="absolute inset-0 bg-gradient-to-r from-(--main-800)/70 via-(--main-700)/40 to-(--main-800)/60"/>
+                <div className="absolute inset-0 bg-radial-[at_30%_40%] from-transparent via-transparent to-(--main-900)/50" />
             </div>
             <div className="relative p-2 h-full z-20 flex flex-col gap-5 lg:gap-20 md:w-full sm:text-center">
                 <h1 className="
