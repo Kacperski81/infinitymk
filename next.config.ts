d6c630470e18709af8a36c8612b15397446412b2
Kacperski81 <kacperski81@gmail.com>
2026-03-02 12:20:35 +0000

    htaccess

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,19 @@
 import type { NextConfig } from "next";
 
+const isFTP = process.env.DEPLOY_TARGET === "ftp";
+
 const nextConfig: NextConfig = {
   /* config options here */
-  cacheComponents: true,
+  ...(!isFTP && { cacheComponents: true }),
   images: {
     formats: ["image/avif", "image/webp"],
     qualities: [75, 80],
-  }
+    ...(isFTP && { unoptimized: true }),
+  },
+  ...(isFTP && {
+    output: "export",
+    trailingSlash: true,
+  }),
 };
 
 export default nextConfig;
